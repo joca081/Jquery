39c1614560a182fcc483e855e6bc2b9d49743d59
joca081 <tidws082@gmail.com>
2025-02-25 14:17:40 -0300

    New commit

---
+++ main.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+    $('header button').click(function() {
+        $('form').slideDown();
+    })
+
+    $('#botao-cancelar').click(function() {
+        $('form').slideUp();
+    })
+
+    $('form').on('submit', function(e) {
+        e.preventDefault();
+        const enderecoDaNovaImagem = $('#endereco-imagem-nova').val(); //para recuperar o valor do iput usa o .val() porque é um input
+        const novoItem = $('<li></li>');
+        $(`<img src="${enderecoDaNovaImagem}" />`).appendTo(novoItem);
+        $(`
+            <div class ="overlay-imagem-link">
+                <a href="${enderecoDaNovaImagem}" target="_blank" tittle="Ver Imagem em Tamanho Real">
+                    Ver imagem tamanho real
+                </a>
+            </div>            
+        `).appendTo(novoItem);
+        $(novoItem).appendTo('ul');
+    })
+})
